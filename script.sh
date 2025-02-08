5da4dafcb41ba0f421e40319263e9af7e434bcb2
Liolax <donleonkot@gmail.com>
2025-02-08 23:51:26 +0000

    Update script.sh to set permissions for files in fol_1 and fol_2

--- script.sh
+++ script.sh
@@ -4,3 +4,7 @@ mkdir fol_1 fol_2
 mkdir -p fol_1 fol_2
 touch fol_1/1_1.txt fol_1/1_2.txt fol_1/1_3.txt
 touch fol_2/2_1.txt fol_2/2_2.txt fol_2/2_3.txt
+chmod 600 fol_1/1_1.txt fol_1/1_3.txt
+chmod 777 fol_1/1_2.txt
+chmod 600 fol_2/2_1.txt fol_2/2_3.txt
+chmod 777 fol_2/2_2.txt
